a331a4957e61a4355003866eccfc619fda9a2e25
subasri s <smsubasri555@gmail.com>
2026-06-05 18:18:48 +0530

    Create App component with AboutUs and ProductList

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import AboutUs from "./AboutUs";
+import ProductList from "./ProductList";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Smart Campus Navigation System</h1>
+
+      <AboutUs />
+
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
