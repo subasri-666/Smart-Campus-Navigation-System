9356b4c6f8d278be1b7112570343d44bec71e7b5
subasri s <smsubasri555@gmail.com>
2026-06-05 18:16:54 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about">
+      <h2>About Smart Campus Navigation</h2>
+      <p>
+        Smart Campus Navigation System helps students and visitors easily
+        locate departments, classrooms, laboratories, library, canteen,
+        auditorium, and administrative offices within the campus.
+      </p>
+
+      <p>
+        Our goal is to reduce confusion and improve accessibility by providing
+        quick and accurate navigation assistance.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
